3e5bb326c85011519557e842d693d43a434427a7
hiddennightcrow <57255290+hiddennightcrow@users.noreply.github.com>
2019-12-20 21:37:27 +0800

    進度2

---
+++ UnityProject/Assets/scrip/player.cs
@@ -0,0 +1,38 @@
+﻿
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Joystick joystick;
+    public Rigidbody rig;
+    private Animator ani;
+    private Transform target;
+
+    public float speed = 10;
+    private void Start()
+    {
+        rig=GetComponent<Rigidbody>();
+        ani = GetComponent<Animator>();
+        joystick =GameObject.Find("虛擬搖桿").GetComponent<Joystick>();
+        target = GameObject.Find("目標").transform;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float v = joystick.Vertical;
+        float h = joystick.Horizontal;
+
+        rig.AddForce(-h*speed, 0, -v*speed);
+        ani.SetBool("跑步開關",v != 0 || h != 0);
+
+        Vector3 posPlaer =transform.position;
+        Vector3 posTarget = new Vector3(posPlaer.x - h, 0.279f, posPlaer.z - v);
+        target.position = posTarget;
+
+        posTarget.y = posPlaer.y;
+        transform.LookAt(posTarget);
+    }
+}
